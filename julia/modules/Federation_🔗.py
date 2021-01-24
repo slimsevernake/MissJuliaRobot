6ed8eda5a9f21142d4ecf489698c21b5ae7145db
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-01-24 20:53:53 +0530

    Update Federation_🔗.py

--- julia/modules/Federation_🔗.py
+++ julia/modules/Federation_🔗.py
@@ -864,3 +864,23 @@ async def _(event):
         await event.reply(
             "Please give some rules to set.")
 
+"""
+ - /newfed <fed_name>: Creates a Federation, one allowed per user.
+ - /renamefed <fed_id> <new_fed_name>: Renames the fed id to a new name.
+ - /delfed <fed_id>: Delete a Federation, and any information related to it. 
+ - /fpromote <user>: Assigns the user as a federation admin. 
+ - /fdemote <user>: Drops the user from the federation admin to a normal user.
+ - /fban (<user>|<reason>): Fed bans a user. Syntax: `fban 12345 | testing`, `fban @MissJuliaRobot | testing`.
+ - /unfban <user> <reason>: Removes a user from a fed ban.
+ - /fedinfo <fed_id>: Information about the specified Federation.
+ - /joinfed <fed_id>: Join the current chat to the Federation. 
+ - /leavefed <fed_id>: Leave the Federation given. 
+ - /setfrules <rules>: Arrange Federation rules.
+ - /fedadmins: Show Federation admin.
+ - /fbanlist: Displays all users who are victimized at the Federation at this time.
+ - /fedchats: Get all the chats that are connected in the Federation.
+ - /chatfed : See the Federation in the current chat.
+ - /fbanstat: Shows if you/or the user you are replying to or their username/id is fbanned somewhere or not.
+ - /fednotif <on/off>: Should the bot send notifications for fban/unfban in PM.
+ - /frules: See the current federation rules.
+"""
