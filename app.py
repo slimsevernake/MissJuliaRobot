154b7f566a289eb8b32b22767d0d60d47d8409be
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-01-27 10:52:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from julia import HEROKU_APP_NAME
 class MissJuliaRobot:
     def on_get(self, req, resp):
         if not req.params:
-            resp.content_type = "text/html"
+            resp.content_type = "application/javascript"
             resp.status = falcon.HTTP_200
             with io.open("index.html", "rb") as f:
                 resp.body = f.read()
