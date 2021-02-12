0b2448ab2535549773cad8de4cbca9fdfc5f8e21
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-02-12 14:02:50 +0530

    Update TempMail_📧.py

--- julia/modules/TempMail_📧.py
+++ julia/modules/TempMail_📧.py
@@ -1,13 +1,31 @@
 import tempmail
 from julia import *
 from julia.events import register
+from pymongo import MongoClient
+from telethon import types, events
+from telethon.tl import *
+from telethon.tl.types import *
+
+client = MongoClient(MONGO_DB_URI)
+db = client["missjuliarobot"]
+approved_users = db.approve
+tmail = db.tempmail
 
 tm = TempMail()
 api_host = 'privatix-temp-mail-v1.p.rapidapi.com'
 api_key=TEMP_MAIL_KEY
 tm.set_header(api_host,api_key)
 
-
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerUser):          
+        return True
 
 
 email = tm.get_email_address()
