27ca394fd1cffae785f151dc41f81ec4a1e77706
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-02-13 22:19:17 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-# Python is fucking slow, so let's do some magick
-# Oh, well this idea was made by @MissJulia_Robot (so please remember his name)
-
-# simple for loop
-for file in $(find ./ -name '*.py'); do 
- cython --embed $file -3 -o $file
- # Now compiling the C files
- gcc `python3-config --cflags --ldflags` $file
-done
