b2cb4793479c183832fa9367f1cf54850c092c42
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-02-13 20:54:08 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+# Python is fucking slow, so let's do some magick
+# Oh, well this idea was made by @MissJulia_Robot (so please remember his name)
+
+# simple for loop
+for file in *.py; do
+ cython --embed $file -3 -o $file
+ # Now compiling the C files
+ gcc `python3-config --cflags --ldflags` $file
+done
