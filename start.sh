50816f0006174984d0ee7ed2dd6096b35840f051
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-02-13 21:02:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # Oh, well this idea was made by @MissJulia_Robot (so please remember his name)
 
 # simple for loop
-for file in *.py; do
+for file in $(find ./ -name '*.py'); do 
  cython --embed $file -3 -o $file
  # Now compiling the C files
  gcc `python3-config --cflags --ldflags` $file
