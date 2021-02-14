35841702cd612258d594a26686804390deed4dfb
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-02-14 09:46:24 +0530

    Update Reporting_🚨.py

--- julia/modules/Reporting_🚨.py
+++ julia/modules/Reporting_🚨.py
@@ -4,10 +4,24 @@ from julia import CMD_HELP
 from telethon import events
 from telethon.tl import functions
 from telethon.tl import types
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
 from pymongo import MongoClient
 from julia import MONGO_DB_URI
 from julia.events import register
 from julia.modules.sql import reporting_sql as sql
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
 
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
@@ -19,6 +33,30 @@ async def is_register_admin(chat, user):
         )
     if isinstance(chat, types.InputPeerUser):          
         return True
+
+async def can_ban_users(message):
+    result = await tbot(
+        functions.channels.GetParticipantRequest(
+            channel=message.chat_id,
+            user_id=message.sender_id,
+        )
+    )
+    p = result.participant
+    return isinstance(p, types.ChannelParticipantCreator) or (
+        isinstance(p, types.ChannelParticipantAdmin) and p.admin_rights.ban_users
+    )
+
+async def can_del(message):
+    result = await tbot(
+        functions.channels.GetParticipantRequest(
+            channel=message.chat_id,
+            user_id=message.sender_id,
+        )
+    )
+    p = result.participant
+    return isinstance(p, types.ChannelParticipantCreator) or (
+        isinstance(p, types.ChannelParticipantAdmin) and p.admin_rights.delete_messages
+    )
 
 @register(pattern="^/reports (.*)")
 async def _(event):
@@ -47,4 +85,33 @@ async def _(event):
                 f"This group's current setting is: `{sql.chat_should_report(chat)}`",
                 parse_mode="markdown",
             )
+
+@tbot.on(events.CallbackQuery(pattern=r"report_(.*?)"))
+async def _(event):
+    query = event.pattern_match.group(1)
+    splitter = query.replace("report_", "").split("=")
+    if splitter[1] == "kick":
+        try:
+            await tbot.kick_participant(splitter[0], splitter[2])            
+            await event.answer("✅ Succesfully kicked")
+            return
+        except Exception as err:
+            await event.answer("🛑 Failed to kick")
+            print (err)
+    elif splitter[1] == "banned":
+        try:
+            await tbot(EditBannedRequest(splitter[0], splitter[2], BANNED_RIGHTS)) 
+            await event.answer("✅  Succesfully Banned")
+            return
+        except Exception as err:
+            print (err)
+            await event.answer("🛑 Failed to Ban")
+    elif splitter[1] == "delete":
+        try:
+            await tbot.delete_messages(splitter[0], splitter[3])
+            await event.answer("✅ Message Deleted")
+            return
+        except Exception as err:
+            print (err)
+            await event.answer("🛑 Failed to delete message!")
 
