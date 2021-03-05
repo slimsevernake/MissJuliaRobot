232ff3d7f01ea40bbeae27ec71da8b4ca2219202
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-03-05 21:48:24 +0530

    Update Stickers_🦎.py

--- julia/modules/Stickers_🦎.py
+++ julia/modules/Stickers_🦎.py
@@ -503,7 +503,7 @@ async def _(event):
                 )
                 return
             try:
-             await ubot.forward_messages("@Stickers", reply_message)
+             await ubot.forward_messages(bot_conv, reply_message)
             except Exception as e:
              print(e)
             if response.text.startswith("This pack has only"):
