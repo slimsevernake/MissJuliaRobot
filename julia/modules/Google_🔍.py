d6085c58faded16e747d761ec6eac6a4645302d4
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-03-12 20:48:18 +0530

    Update Google_🔍.py

--- julia/modules/Google_🔍.py
+++ julia/modules/Google_🔍.py
@@ -42,10 +42,10 @@ async def is_register_admin(chat, user):
     if isinstance(chat, types.InputPeerUser):
         return True
 
-def google_search(search_term, api_key, cse_id, kwargs):
-      service = build("customsearch", "v1", developerKey=api_key)
-      res = service.cse().list(q=search_term, cx=cse_id, kwargs).execute()
-      return res['items']
+def google_search(search_term, api_key, cse_id, **kwargs):
+ service = build("customsearch", "v1", developerKey=api_key)
+ res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+ return res['items']
 
 @register(pattern="^/google (.*)")
 async def _(event):
